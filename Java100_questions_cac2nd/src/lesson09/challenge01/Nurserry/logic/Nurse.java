db9e6fc0dca010ab8eb1f53db8cf9a7e748cd0f9
y-yasuda <y-yasuda@CD26311L.arksystems.co.jp>
2026-04-24 16:01:48 +0900

    9-1～9-4

--- Java100_questions_cac2nd/src/lesson09/challenge01/Nurserry/logic/Nurse.java
+++ Java100_questions_cac2nd/src/lesson09/challenge01/Nurserry/logic/Nurse.java
@@ -1,5 +1,6 @@
 package lesson09.challenge01.Nurserry.logic;
 
+
 public class Nurse {
     private String name;
 
@@ -17,9 +18,11 @@ public class Nurse {
 
     public void takeAttendance(NurserySchoolChild[] childs, int num) {
 
+        try{
+            System.out.println(childs[num-1].getName()+"は出席しています。");
 
-       //ここに適切な処理を記述
-
-
+        }catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("その番号の園児は出席してません。");
+        }
     }
 }
--- Java100_questions_cac2nd/src/lesson09/challenge03/Nurserry/logic/Nurse.java
+++ Java100_questions_cac2nd/src/lesson09/challenge03/Nurserry/logic/Nurse.java
@@ -1,25 +1,33 @@
 package lesson09.challenge03.Nurserry.logic;
 
 public class Nurse {
-    private String name;
+	private String name;
 
-    public Nurse(String name) {
-        this.name = name;
-    }
+	public Nurse(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void divide(int cakes, int childs) {
+	public void divide(int cakes, int childs) {
 
+		//ここに適切な処理を記述
 
-        //ここに適切な処理を記述
+		try {
+			int cakePer = cakes / childs;
+			System.out.println("1人" + cakePer + "個になります。");
+		} catch (ArithmeticException e) {
+			System.out.println("ケーキを食べる園児がいません。");
+		} finally {
+			System.out.println("ケーキの分配が終わりました。");
+		}
 
+	}
 
-    }
 }
